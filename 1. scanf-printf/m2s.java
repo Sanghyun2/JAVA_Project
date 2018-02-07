69cc999078f061aaf8d9c595449335baae44c6c6
Sanghyun2 <33412503+Sanghyun2@users.noreply.github.com>
2018-02-07 13:24:50 +0900

    Create m2s.java

---
+++ 1. scanf-printf/m2s.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class Main {  // calculate the second
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int m = scanner.nextInt();
+		System.out.println(m+" minutes is "+m*60+" seconds.");
+	}
+}

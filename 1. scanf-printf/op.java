cfc75442732cd2c28571542218bf43fd3fa52726
Sanghyun2 <33412503+Sanghyun2@users.noreply.github.com>
2018-02-07 13:31:47 +0900

    Update op.java

--- 1. scanf-printf/op.java
+++ 1. scanf-printf/op.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-public class Main {  // calculate the second
+public class Main {  // calculate the basic operation
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
